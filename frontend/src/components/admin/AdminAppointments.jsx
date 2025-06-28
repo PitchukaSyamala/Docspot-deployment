28d9239b33de83b8ef6a570ce16b6175a11f92fc
PITCHUKA SYAMALA <161848630+PitchukaSyamala@users.noreply.github.com>
2025-06-28 17:58:43 +0530

    Update AdminAppointments.jsx

--- frontend/src/components/admin/AdminAppointments.jsx
+++ frontend/src/components/admin/AdminAppointments.jsx
@@ -10,7 +10,7 @@ const AdminAppointments = () => {
 
    const getAppointments = async () => {
       try {
-         const res = await axios.get('http://localhost:5002/api/admin/getallAppointmentsAdmin', {
+         const res = await axios.get('https://docspot-deployment.onrender.com/admin/getallAppointmentsAdmin', {
             headers: {
                Authorization: `Bearer ${localStorage.getItem('token')}`
             },
